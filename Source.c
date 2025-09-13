ce32e43556f90483d96160a6b549e7b5d1c990f1
Юлия <uleckabelebezeva@gmail.com>
2025-09-13 15:39:13 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,21 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts("         /\\  ___  /\\    ");
+	puts("        /  _     _  \\    ");
+	puts("       |   _0_  _0_  |    ");
+	puts("       ( =     ^     )=   ");
+	puts("        /            ]    ");
+	puts("       /             ]    ");
+	puts("       [     \\   |  |    ");
+	puts("       /      |  |  |     ");
+	puts("     __|      |  |  |     ");
+	puts("    /   __    |  |  |     ");
+	puts("    |  (   \\__\\_ )__)   ");
+	puts("    |  (_                 ");
+	puts("    \\_____)              ");
+	getchar();
+}
